5bdfaf329a3ae9943de5cc313251b4cd37520dc7
LeeJooHyeob <jobkid@naver.com>
2022-07-06 15:50:54 +0900

    0706LoginJoin

---
+++ src/main/java/DAO/MemberDao.java
@@ -0,0 +1,131 @@
+package DAO;
+import java.sql.*;
+import beans.Member;
+
+public class MemberDao {
+	private MemberDao(){
+		
+	}//다른 곳에서 생성자를 쓰지 못하게 막는다.
+	private static MemberDao instance = new MemberDao();
+	//생성자는 하나 밖에 만들지 못한다. 그래도 생성자를 써야하기 때문에 static으로 잡는다.
+	
+	public static MemberDao getInstance() {
+		return instance;
+	}
+	//케넥션
+	public Connection getConnection() throws Exception {
+		Connection conn=null;
+		String url="jdbc:mysql://127.0.0.1:3306/study";
+		String db_id="root";
+		String db_pw="iotiot";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn=DriverManager.getConnection(url, db_id, db_pw);
+		
+		return conn;
+	}
+	
+	//사용자 인증처리(로그인)
+	public int userCheck(String userid, String pwd) {
+		int result=-1;
+		String sql="select pwd from member where userid=?";
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {//해당 아이디가 존재하는 경우
+				if(rs.getString("pwd").equals(pwd)&&rs.getString("pwd")!=null) {
+					//아이디가 존재하고 비밀번호가 일치하는 경우
+					result=1;
+				}else {//비밀번호가 없거나 일치하지 않는경우
+					result=0;
+				}
+			}else {//해당 아이디가 존재하지 않는 경우
+				result=-1;
+			}
+		}catch(Exception e) {
+			System.out.println("로그인 중 오류 발생 : "+e);
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception ex) {
+				System.out.println("로그인 회선 종료 중 오류 발생 :"+ex);
+			}
+		}
+		
+		return result;//계산된 결과를 return
+	}
+	//사용자 정보 조회
+	public Member getMember(String userid) {
+		Member m=null;
+		String sql="select * from member where userid=?";
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				m=new Member();
+				m.setNum(rs.getString("num"));
+				m.setName(rs.getString("name"));
+				m.setUserid(rs.getString("userid"));
+				m.setPwd(rs.getString("pwd"));
+				m.setEmail(rs.getString("email"));
+				m.setPhone(rs.getString("phone"));
+				m.setAdmin(rs.getString("admin"));
+			}
+		}catch(Exception e) {
+			System.out.println("멤버정보 조회 중 오류 발생 : "+e);
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception ex) {
+				System.out.println("멤버정보 조회 종료 중 오류 발생 : "+ex);
+			}
+		}
+		return m;
+	}
+	//아이디 중복체크 처리를 진행할 메서드
+	public int confirmID(String userid) {
+		int result=1;
+		String sql="select userid from member where userid=?";
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				result=1;
+			}else {
+				result=-1;
+			}
+		}catch(Exception e) {
+			System.out.println("MemberDao.confirmID()에서 접속 중 오류발생 : "+e);
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception ex) {
+				System.out.println("MemberDao.confirm()에서 접속 후");
+			}
+			
+		}
+		return result;
+	}
+}
---
+++ src/main/java/controller/JoinServlet.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class joinServlet
+ */
+@WebServlet("/join.do")
+public class JoinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+	//127.0.0.1:8080/Board01/join.do. a태그 또는 form method="get"으로 실행 시 실행되는 부분
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dis = request.getRequestDispatcher("/register.jsp");
+		dis.forward(request, response);
+	}
+
+	//127.0.0.1:8080/Board01/join.do. a태그 또는 form method="post"으로 실행 시 실행되는 부분
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String name=request.getParameter("name");
+		String userid=request.getParameter("userid");
+		String pwd=request.getParameter("pwd");
+		String email=request.getParameter("email");
+		String phone=request.getParameter("phone");
+		String admin=request.getParameter("admin");
+		System.out.println(name+", "+userid+", "+pwd+", "+email);
+	}
+
+}
+
